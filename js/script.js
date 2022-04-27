7c27bc07067f9c39503e167b06db9cbc6bd4a874
daeun34 <103724161+daeun34@users.noreply.github.com>
2022-04-27 17:51:47 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100                                                          ) {
-        $('.top').fadeIn();
-    } else {
-        $('.top').fadeOut();
-    }
-	var height = $(window).scrollTop();
-    if (height > 100                                                          ) {
-        $('.booking').fadeIn();
-    } else {
-        $('.booking').fadeOut();
-    }
+$(function(){
+	AOS.init();	
 });

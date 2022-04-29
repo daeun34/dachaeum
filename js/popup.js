c1094c3af9e7b3e3a744bfdb9f0b8d75db105cc4
daeun34 <103724161+daeun34@users.noreply.github.com>
2022-04-29 16:04:42 +0900

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,20 @@
 $(function(){
-    $(".wrap a").click(function(){
-        $(".popup").fadeIn(); //show();
+    $(".a1").click(function(){
+        $(".popup1").fadeIn();//show();
     });
-
-    $(".popup a").click(function(){
-        $(".popup").fadeOut();  //hide();
+    $(".popup1 a").click(function(){
+        $(".popup1").fadeOut();//hide();
+    });
+    $(".a2").click(function(){
+        $(".popup2").fadeIn();//show();
+    });
+    $(".popup2 a").click(function(){
+        $(".popup2").fadeOut();//hide();
+    });
+    $(".a4").click(function(){
+        $(".popup4").fadeIn();//show();
+    });
+    $(".popup4 a").click(function(){
+        $(".popup4").fadeOut();//hide();
     });
 });
